ecf3f39fb437a8d2277606430c065952f9cb2326
Aziz Ahmad <khatirjan294926@gmail.com>
2026-03-10 19:00:17 +0500

    Delete lab 4 directory

--- lab 4/task 1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream> // Header file for input and output
-using namespace std; // Use standard namespace
-int main() // Main function starts execution
-{
- cout << "Hello, World!"; // Print Hello, World on screen
- 
- return 0; // Return 0 means program executed successfully
-
-}
