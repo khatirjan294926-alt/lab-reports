94082f0e1398eeba4d57f44f266596c912b8dbd3
Aziz Ahmad <khatirjan294926@gmail.com>
2026-05-12 13:51:06 +0500

    Add files via upload

---
+++ lab 11/tasks/task 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    // Declare and initialize strings
+    string firstName = "Aziz";
+    string lastName = "Ahmad";
+
+    // Concatenate strings
+    string fullName = firstName + " " + lastName;
+
+    // Display result
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Full Name: " << fullName << endl;
+
+    return 0;
+}
